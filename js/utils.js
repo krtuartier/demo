435af18dc356548f82ced58727e9e327d7a635fe
krtuartier <krtuartier@gmail.com>
2017-09-18 19:26:38 +0800

    up

--- js/utils.js
+++ js/utils.js
@@ -8,6 +8,21 @@ function RootNodeFont() {
 		this.html.style.fontSize = this.hWidth / 7.5 + "px";
 	}
 }
-window.onload = function() {
-	RootNodeFont();
-}
+
+(function($) {
+	//jQrem
+	$.fn.extend({
+		AutoSize: function() {
+			var element = $(this);
+			auto();
+
+			function auto() {
+				var width = $(window).width(),
+					height = $(window).height();
+				$("html").css("font-size", width / 7.5);
+				$(element).width(width).height(height);
+			};
+			$(window).resize(auto);
+		}
+	});
+})(jQuery);
